f15ffd7635d962968d5374b276a9bc7180ea022c
Stefan Arentz <sarentz@mozilla.com>
2012-11-15 21:18:56 -0500

    Closes #28 Add a list of known services and their risk level to the NMAP plugin

--- plugins/nmap/minion/plugins/nmap.py
+++ plugins/nmap/minion/plugins/nmap.py
@@ -5,22 +5,105 @@
 
 import re
 import urlparse
-from minion.plugin_api import AbstractPlugin, ExternalProcessPlugin
+from minion.plugin_api import ExternalProcessPlugin
 
+NOTABLE_ISSUES = [
+    {
+        "_ports": [22],
+        "Severity": "Low",
+        "Summary": "Public SSH service found"
+    },
+    {
+        "_ports": [53],
+        "Severity": "Low",
+        "Summary": "Public DNS service found"
+    },
+    {
+        "_ports": [80,443],
+        "Severity": "Informational",
+        "Summary": "Standard HTTP services were found"
+    },
+    {
+        "_ports": [3306],
+        "Ports": [],
+        "Severity": "High",
+        "Summary": "Public MySQL database found",
+        "Description": "A publicly accessible instance of the MySQL database was found on port 3306.",
+        "Solution": "Configure MySQL to listen only on localhost. If other servers need to access to this database then use firewall rules to only allow those servers to connect."
+    },
+    {
+        "_ports": [5432],
+        "Severity": "High",
+        "Summary": "Public PostgreSQL database found",
+        "Description": "A publicly accessible instance of the PostgreSQL database was found on port 5432.",
+        "Solution": "Configure PostgreSQL to listen only on localhost. If other servers need to access this database then use firewall rules to only allow those servers to connect."
+    },
+    {
+        "_ports": [25,113,143,465,587,993,995],
+        "Severity": "Medium",
+        "Summary": "Email service(s) found",
+        "Solution": "It is not recomended to run email services on the same server on which a web site is hosted. It is generally a good idea to separate services to different servers to minimize the attack surface."
+    }
+]
+
+def find_notable_issue(port):
+    for issue in NOTABLE_ISSUES:
+        if port in issue['_ports']:
+            return issue
 
 def parse_nmap_output(output):
-    ports = []
+    services = []
     for line in output.split("\n"):
         match = re.match('^(\d+)/(tcp|udp)\s+open\s+(\w+)', line)
         if match is not None:
-            #ports.append({'port':match.group(1),'protocol':match.group(2), 'service':match.group(3)})
-            port = int(match.group(1))
-            severity = 'High'
-            if port in (80,443):
-                severity = 'Info'
-            ports.append({'Summary': 'Port %d is open' % port, 'Severity': severity})
-    return ports
+            services.append({'port':int(match.group(1)),'protocol':match.group(2), 'service':match.group(3)})
+    return services
 
+def find_port_in_issues(port, issues):
+    for issue in issues:
+        if port in issue['Ports']:
+            return True
+
+def find_earlier_found_issue(port, issues):
+    for issue in issues:
+        if port in issue['_ports']:
+            return issue
+
+def services_to_issues(services):
+
+    unique_ports = set()
+    for service in services:
+        unique_ports.add(service['port'])
+    
+    high_risk_ports = set()
+
+    issues = []
+
+    for port in unique_ports:
+        # If we have not seen this port before
+        if port not in high_risk_ports and not find_port_in_issues(port, issues):
+            issue = find_earlier_found_issue(port, issues)
+            if issue:
+                issue.setdefault("Ports", []).append(port)                
+            else:
+                issue = find_notable_issue(port)
+                if issue:
+                    # If we have a detailed issue then we use that
+                    issues.append(issue)
+                    issue.setdefault("Ports", []).append(port)
+                else:
+                    # Otherwise all unknown services go to high risk.
+                    high_risk_ports.add(port)
+
+    if len(high_risk_ports) > 0:
+        issues.append({"Ports": list(high_risk_ports), "Severity": "High",
+                       "Summary": "Unknown public services found."})
+        
+    for issue in issues:
+        if '_ports' in issue:
+            del issue['_ports']
+
+    return issues
 
 class NMAPPlugin(ExternalProcessPlugin):
 
@@ -49,5 +132,7 @@ class NMAPPlugin(ExternalProcessPlugin):
         self.output += data
 
     def do_process_ended(self, status):
-        self.callbacks.report_results(parse_nmap_output(self.output))
+        services = parse_nmap_output(self.output)
+        issues = services_to_issues(services)
+        self.callbacks.report_results(issues)
         self.callbacks.report_finish()
