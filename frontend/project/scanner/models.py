e248e1bbb1c11e571e4a1e80ca2b8ec12686ac83
Stefan Arentz <stefan@arentz.ca>
2012-11-20 22:17:35 -0500

    Closes #46 Let Scans reference the User object by primary key

--- frontend/project/scanner/models.py
+++ frontend/project/scanner/models.py
@@ -1,15 +1,9 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-# User class extensions
-def user_unicode(self):
-    """Use email address for string representation of user."""
-    return self.email
-User.add_to_class('__unicode__', user_unicode)
-
 class Scan(models.Model):
     scan_id = models.CharField(max_length=100, primary_key = True)
-    scan_creator = models.CharField(max_length=150)
+    scan_creator = models.ForeignKey(User)
     scan_date = models.DateTimeField(auto_now_add=True)
     scan_url = models.URLField()
     scan_plan = models.CharField(max_length=100)
