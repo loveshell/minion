86c1c4e998a885d87a53daa86ebbcfc0dc6da86e
Stefan Arentz <stefan@arentz.ca>
2012-11-27 14:01:35 -0500

    Closes #56 Add proper license to the project

--- frontend/project/scanner/views.py
+++ frontend/project/scanner/views.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 from django.shortcuts import render, redirect
 from funfactory.log import log_cef
 from django.core.context_processors import csrf
